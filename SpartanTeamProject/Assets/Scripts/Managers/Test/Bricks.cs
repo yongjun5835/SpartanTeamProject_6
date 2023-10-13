05a83d81eff102e327600de3f901111af0bd2767
Son-BE <122617318+Son-BE@users.noreply.github.com>
2023-10-13 11:04:35 +0900

    test

---
+++ SpartanTeamProject/Assets/Scripts/Managers/Test/Bricks.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Bricks : MonoBehaviour
+{
+    public Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
