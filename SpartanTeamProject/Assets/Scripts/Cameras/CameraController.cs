005d9276be52875c2e641a96b8ce1343dc920816
Zif1519 <141081153+Zif1519@users.noreply.github.com>
2023-10-17 18:57:01 +0900

    [UPDATE] Change logics for natural camera moving
    [UPDATE] Change logics for natural camera moving

--- SpartanTeamProject/Assets/Scripts/Cameras/CameraController.cs
+++ SpartanTeamProject/Assets/Scripts/Cameras/CameraController.cs
@@ -9,30 +9,24 @@ public enum CameraModes { Computer, Mobile }
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
-
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private CinemachineConfiner confiner;
     [SerializeField] private Transform curTarget;
 
     [Header("Need to connect")]
-
-    // 여기도 임시
     [SerializeField] private Transform player;
-    //
-    [SerializeField] private Transform freeCameraTarget;
-    [SerializeField] private UI_OutOfCamera ui_OutOfCamera;
+
+    // 작업중
+    // [SerializeField] private UI_OutOfCamera ui_OutOfCamera;
+    [Header("Settings")]
+    public float zoomMaxSize = 10.0f;
+    public float zoomMinSize = 5.0f;
+    public float freeCameraMoveSpeed = 10.0f;
+    public Camera mainCamera;
     private bool isTargetOutOfCamera;
     private bool isFreeCameraMode;
+    private Bounds bounds;
 
-    public float scrollSpeed = 30.0f;
-    public float maxSize = 10.0f;
-    public float minSize = 5.0f;
-
-    public float moveSpeed = 10.0f;
-
-    float wheelScrollValue;
-
-    public Camera mainCamera;
     private void Awake()
     {
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
@@ -45,6 +39,7 @@ public class CameraController : MonoBehaviour
         GameManager.Instance.cameraController = this;
         // 임시임
         SetTarget(player);
+        MapChange(confiner.m_BoundingShape2D);
         // 여기까지
     }
 
@@ -64,11 +59,12 @@ public class CameraController : MonoBehaviour
         }
         MouseWheelCheck();
     }
-    public void ChangeBounds(Collider2D collider)
+    public void MapChange(Collider2D map_PolygonCollider)
     {
-        if (collider != null)
+        if (map_PolygonCollider != null)
         {
-            confiner.m_BoundingShape2D = collider;
+            confiner.m_BoundingShape2D = map_PolygonCollider;
+            bounds = map_PolygonCollider.bounds;
         }
     }
 
@@ -85,9 +81,8 @@ public class CameraController : MonoBehaviour
         if (isFreeCameraMode && !(Cursor.lockState == CursorLockMode.Locked))
         {
             Cursor.lockState = CursorLockMode.Locked;
-            Vector2 position = Camera.main.transform.position;
-            freeCameraTarget.position = position;
-            virtualCamera.m_Follow = freeCameraTarget;
+            virtualCamera.m_Follow = null;
+            virtualCamera.transform.position = Camera.main.transform.position;
         }
         if (!isFreeCameraMode && Cursor.lockState == CursorLockMode.Locked)
         {
@@ -101,17 +96,22 @@ public class CameraController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         Vector3 moveDirection = new Vector3(mouseX, mouseY, 0);
-        freeCameraTarget.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        Vector3 newPosition = virtualCamera.transform.position + moveDirection * freeCameraMoveSpeed * Time.deltaTime;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, bounds.min.x, bounds.max.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, bounds.min.y, bounds.max.y);
+
+        virtualCamera.transform.position = newPosition;
     }
 
     public void MouseWheelCheck()
     {
-        wheelScrollValue = Input.GetAxis("Mouse ScrollWheel");
+        float wheelScrollValue = Input.GetAxis("Mouse ScrollWheel");
         if (wheelScrollValue != 0)
         {
             float newSize = virtualCamera.m_Lens.OrthographicSize;
             newSize += wheelScrollValue > 0 ? -1f : 1f;
-            newSize = Mathf.Clamp(newSize, minSize, maxSize);
+            newSize = Mathf.Clamp(newSize, zoomMinSize, zoomMaxSize);
             virtualCamera.m_Lens.OrthographicSize = newSize;
         }
     }
