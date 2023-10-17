84ef65cf366f47106834b06eb6b5e256f3c730cc
Jo Yongjun <yongjun5835@gmail.com>
2023-10-17 11:55:40 +0900

    플레이어 이동과 이동거리 제한 구현

--- SpartanTeamProject/Assets/Scripts/PlayerController.cs
+++ SpartanTeamProject/Assets/Scripts/PlayerController.cs
@@ -14,10 +14,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float maxPower = 100;
     public float currentPower = 0;
+    public float moveLimit = 5;
 
     private bool isSetDir = false;
     private bool isSetPower = false;
     private bool isShoot = false;
+
+    private Vector3 startPos;
 
     [HideInInspector]
     public Vector2 v2;
@@ -27,6 +30,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        startPos = transform.position;
     }
 
     private void Start()
@@ -71,6 +75,8 @@ public class PlayerController : MonoBehaviour
             // 발사 로직
             ProjectileManager.instance.Shoot();
         }
+
+        Move();
     }
 
     public void SettingDir()
@@ -104,5 +110,15 @@ public class PlayerController : MonoBehaviour
         currentPower = 0;
         Crosshair.gameObject.SetActive(false);
         testBtn.GetComponent<Button>().interactable = true;
+    }
+
+    public void Move()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal") * 5 * Time.deltaTime;
+        if (transform.position.x - startPos.x > moveLimit && xMove > 0)
+            xMove = 0;
+        if (transform.position.x - startPos.x < -moveLimit && xMove < 0)
+            xMove = 0;
+        transform.position += new Vector3(xMove, 0, 0);
     }
 }
