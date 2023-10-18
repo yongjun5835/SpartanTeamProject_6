062faad55fc8ff8044ece4cb23ac7b9ea3225c4a
Zif1519 <141081153+Zif1519@users.noreply.github.com>
2023-10-18 16:32:00 +0900

    [FIX] Enemydefault power
    [FIX] Enemydefault power

--- SpartanTeamProject/Assets/Scripts/Enemy/StateMachines/EnemyAttackState.cs
+++ SpartanTeamProject/Assets/Scripts/Enemy/StateMachines/EnemyAttackState.cs
@@ -8,12 +8,9 @@ public class EnemyAttackState : EnemyGroundState
     }
     public override void Enter()
     {
-        Debug.Log("ø©±‚ »£√‚µ≈?1");
         base.Enter();
-        Debug.Log("ø©±‚ »£√‚µ≈?2");
         stateMachine.Enemy.Animator.SetTrigger(stateMachine.Enemy.AnimData.AttackParameterHash);
-        Debug.Log("ø©±‚ »£√‚µ≈?3");
-        ProjectileManager.instance.EnemyShoot(0, 10f, stateMachine.Enemy.transform.position);
+        ProjectileManager.instance.EnemyShoot(0, 20f, stateMachine.Enemy.transform.position);
     }
 
     public override void Exit()
